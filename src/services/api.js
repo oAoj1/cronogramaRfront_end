a44bdc9e40f1eb32163ce5d9bfe0d786760fb84b
oAoj1 <joaogbmarques@gmail.com>
2023-06-16 11:16:58 -0300

    alterando URL da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:'http://localhost:5555'
+    baseURL:'https://cronogramarbackend-production.up.railway.app'
 })
 
 export default api
