5931e8e04a58ba2b503b25efc91a4c89d0b920fb
oAoj1 <joaogbmarques@gmail.com>
2023-06-20 18:46:10 -0300

    Update index.jsx

--- src/components/infoDay/index.jsx
+++ src/components/infoDay/index.jsx
@@ -9,24 +9,63 @@ export default function InfoDay(){
     const [periodoAgora,setPeriodoAgora] = useState('')
 
     useEffect(() => {
+        var data = new Date()
 
         async function lerDia(){
-          const response = await api.get('/tempo/dia')
-          const data = response.data
+
+          let diaHoje = data.getDay()
+
+          if(diaHoje == 0){
+              diaHoje = 'Domingo'
+          }
+          if(diaHoje == 1){
+              diaHoje = 'Segunda-feira'
+          }
+          if(diaHoje == 2){
+              diaHoje = 'Terça-feira'
+          }
+          if(diaHoje == 3){
+              diaHoje = 'Quarta-feira'
+          }
+          if(diaHoje == 4){
+              diaHoje = 'Quinta-feira'
+          }
+          if(diaHoje == 5){
+              diaHoje = 'Sexta-feira'
+          }
+          if(diaHoje == 6){
+              diaHoje = 'Sábado'
+          }
 
           setDia(data)
         }
     
         async function lerData(){
-          const response = await api.get('/tempo/data')
-          const data = response.data
+          let dia = String(data.getDate()).padStart(2,'0')
+          let mes = String(data.getMonth() + 1).padStart(2,'0')
+
+          let dataCompleta = `${dia}/${mes}`
 
           setData(data)
         }
     
         async function lerPeriodoAgora(){
-          const response = await api.get('/tempo/periodoagora')
-          const data = response.data
+          let hora = data.getHours()
+          var periodoAgora = ''
+
+          if(hora >= 0 && hora <= 5){
+              periodoAgora = 'Madrugada'
+              
+          }else if(hora >= 6 && hora <= 11){
+              periodoAgora = 'Manhã'
+              
+          }else if(hora >= 12 && hora <= 17){
+              periodoAgora = 'Tarde'
+
+          }else if(hora >= 18 && hora <= 23){
+              periodoAgora = 'Noite'
+
+          }
 
           setPeriodoAgora(data)
         }
